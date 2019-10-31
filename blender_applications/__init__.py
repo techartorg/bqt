81d49bc48fc37d110bbab034d8c928d30b99194a
Greg Amato <amatobahn@gmail.com>
2019-10-30 21:29:19 -0500

    Refactored code to not duplicate functions needed in base abstract class

--- blender_applications/__init__.py
+++ blender_applications/__init__.py
@@ -3,5 +3,6 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
-
-from .Win32BlenderApplication import Win32BlenderApplication
+from .blender_application import BlenderApplication
+from .win32_blender_application import Win32BlenderApplication
+from .darwin_blender_application import DarwinBlenderApplication

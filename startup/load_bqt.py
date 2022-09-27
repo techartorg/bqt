3237cf675f4ff5c76d78ebb92f16c9d02ade26e7
Frieder Erdmann <55715326+friedererdmann@users.noreply.github.com>
2022-09-27 15:55:17 +0200

    Files to make testing bqt fast and easy

---
+++ startup/load_bqt.py
@@ -0,0 +1,26 @@
+from logging import root
+import sys
+import os
+import inspect
+
+file_name = inspect.getsourcefile(lambda:0)
+root_path = os.path.abspath(os.path.join(os.path.dirname(file_name), os.pardir))
+site_packages = os.path.join(root_path, "env", "Lib", "site-packages")
+
+# if you do not want to install bqt to the site-packages, the root needs to be on sys.path
+# sys.path.append(root_path)
+sys.path.append(site_packages)
+
+import bqt
+
+bqt.register()
+bqt.create_global_app()
+bqt.instantiate_application()
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass

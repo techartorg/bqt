2ad733e44350a92462423893fd66570d5272f5c8
Frieder Erdmann <frieder.erdmann@massive.se>
2022-10-07 16:49:20 +0200

    some speed up to startup

--- test_bqt.bat
+++ test_bqt.bat
@@ -1,17 +1,19 @@
 echo off
+setlocal enabledelayedexpansion
 set "bln_ver=3.2"
 set "bln_dir=C:\Program Files\Blender Foundation\Blender %bln_ver%"
 set "bln_exe=%bln_dir%\blender.exe"
 set "bln_py=%bln_dir%\%bln_ver%\python\bin\python.exe"
-
-call "%bln_py%" -m venv env
-call "%~dp0env\Scripts\activate"
-
-SET PIP_DISABLE_PIP_VERSION_CHECK=1
-:: dev requirements only for ease of use in ide
-call python -m pip install -r dev-requirements.txt
-call python -m pip install -r requirements.txt
-:: call python -m pip install %~dp0 --use-feature=in-tree-build
+set "venv=%~dp0env\"
+if not exist %venv% (
+    call "%bln_py%" -m venv env
+    call "%~dp0env\Scripts\activate"
+    SET PIP_DISABLE_PIP_VERSION_CHECK=1
+    :: dev requirements only for ease of use in ide
+    call python -m pip install -r requirements.txt
+    :: call python -m pip install %~dp0 --use-feature=in-tree-build
+    call python -m pip install -r dev-requirements.txt
+)
 
 set "BLENDER_USER_SCRIPTS=%~dp0"
-call "%bln_exe%"
+call "%bln_exe%" %*

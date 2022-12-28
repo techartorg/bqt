ff7dd59986f2864d2fa6980c81f7a63aa896c5f6
hannes <hannesdelbeke@gmail.com>
2022-12-28 14:26:42 +0000

    hookup save button, hide window-bar

--- bqt/quit_dialogue.py
+++ bqt/quit_dialogue.py
@@ -1,5 +1,6 @@
 import bpy
 from PySide2.QtWidgets import QMessageBox
+from PySide2.QtCore import Qt
 import os
 
 
@@ -10,44 +11,57 @@ def shutdown_blender(*args):
 
 
 class WINDOW_OT_SaveFileFromQt(bpy.types.Operator):
-    """Saves current Blender file and all modified images"""
     bl_idname = "wm.save_from_qt"
     bl_label = "Save_from_Qt"
 
     def execute(self, context):
         # TODO not sure what we are doing here, Friederman?
         if context.blend_data.is_saved:
-            bpy.ops.wm.save_mainfile('EXEC_AREA', check_existing=False)
+            bpy.ops.wm.save_mainfile({"window": bpy.context.window_manager.windows[0]}, 'EXEC_AREA', check_existing=False)
         else:
-            bpy.ops.wm.save_mainfile('INVOKE_AREA', check_existing=False)
+            bpy.ops.wm.save_mainfile({"window": bpy.context.window_manager.windows[0]}, 'INVOKE_AREA', check_existing=False)
         # https://docs.blender.org/api/current/bpy.ops.html
         # EXEC_AREA - execute the operator in a certain context
         return {'FINISHED'}
 
 
+# todo
+#  darker background
+#  different icon to match the theme
+#  different button UI look
+#  when clicking the icon, the dialogue resets to center screen position
+#  support dragging the dialogue around
+
 class BlenderClosingDialog(QMessageBox):
     def __init__(self, parent):
         super().__init__(parent) #, Qt.WindowCloseButtonHint | Qt.WindowSystemMenuHint | Qt.WindowTitleHint | Qt.WindowStaysOnTopHint)
-        self.setWindowTitle("Save changes before closing?")
+
+        # hide title bar
+        self.setWindowFlag(Qt.FramelessWindowHint)
+
         filepath = bpy.data.filepath
         if not filepath:
             filepath = 'untitled.blend'
         filename = os.path.split(filepath)[1]
-        self.setText(filename)
-        self.setStandardButtons(QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
+
+        # self.setWindowTitle("Save changes before closing?")
+        self.setText("Save changes before closing?\n" + filename)
+        self.setStandardButtons(QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+        self.setDefaultButton(QMessageBox.Save)
         self.setIcon(QMessageBox.Question)
 
     def execute(self):
-        if bpy.data.is_dirty:
+        if not bpy.data.is_dirty:
             shutdown_blender()
             return
 
-        choice = self.exec_()
-        if choice == QMessageBox.Yes:
+        choice = super().exec_()
+        if choice == QMessageBox.Save:
             bpy.utils.register_class(WINDOW_OT_SaveFileFromQt)
             bpy.app.handlers.save_post.append(shutdown_blender)
             bpy.ops.wm.save_from_qt()
-        elif choice == QMessageBox.No:
+        elif choice == QMessageBox.Discard:
             shutdown_blender()
         else:  # user clicked cancel
             pass
+        return choice

ee21b50e0edb229c96d69da81be27143fd39f9e4
hannes <hannesdelbeke@gmail.com>
2023-03-15 19:27:05 +0000

    add encoding to setup to correctly process README

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ import sys
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 
 class CustomInstall(install):

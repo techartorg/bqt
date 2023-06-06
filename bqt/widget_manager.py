e03d7dd5a57b779415fb8e952c6420c8790fbf04
hannes <hannesdelbeke@gmail.com>
2023-06-06 18:39:08 +0100

    handle non window top level qwidgets

--- bqt/widget_manager.py
+++ bqt/widget_manager.py
@@ -124,4 +124,13 @@ def parent_orphan_widgets(exclude=None):
     exclude = exclude or []
     __excluded_widgets.extend(exclude)
     for widget in _orphan_toplevel_widgets():
+
+        # check if widget is window type, else skip and exclude
+        if not widget.windowType() == Qt.Window:
+            __excluded_widgets.append(widget)
+            continue
+        # todo test with various widgets, we likely exclude some valid widgets
+        #  this should fail with a combobox (dropdown) and menu
+
         register(widget, exclude=exclude)
+

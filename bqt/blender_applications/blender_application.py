00756349c03780700c9d66bac9daee73ce845d71
hannes <hannesdelbeke@gmail.com>
2023-06-06 13:07:08 +0100

    ensure we only manage foreground if blender is not wrapped

--- bqt/blender_applications/blender_application.py
+++ bqt/blender_applications/blender_application.py
@@ -81,7 +81,8 @@ class BlenderApplication(QApplication):
 
     def on_update(self):
 
-        if os.getenv("BQT_MANAGE_FOREGROUND", "1") == "1" and self.blender_focus_toggled():
+        # we only need foreground managing if blender is not wrapped
+        if os.getenv("BQT_DISABLE_WRAP") == "1" and os.getenv("BQT_MANAGE_FOREGROUND", "1") == "1" and self.blender_focus_toggled():
             focussed_on_a_blender_window = self._active_window_hwnd != 0  # 0 for windows not created by blender
             bqt.widget_manager._blender_window_change(focussed_on_a_blender_window)
 

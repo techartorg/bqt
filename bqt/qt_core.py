77f6b38052d3bf8062f9ed944b9f022901c791f4
Hannes Delbeke <hannesdelbeke@gmail.com>
2023-12-18 09:54:58 +0000

    Update qt_core.py

--- bqt/qt_core.py
+++ bqt/qt_core.py
@@ -4,12 +4,10 @@ try:
     from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QDockWidget, QMessageBox
     from PySide6.QtGui import QCloseEvent, QIcon, QWindow,  QImage, QPixmap
 except ImportError:
-    pass
-
-try:
-    from PySide2 import QtCore
-    from PySide2.QtCore import Qt, QEvent, QObject, QRect, QSettings, QTimer, QDir
-    from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QDockWidget, QMessageBox
-    from PySide2.QtGui import QCloseEvent, QIcon, QWindow,  QImage, QPixmap
-except ImportError:
-    pass
+    try:
+        from PySide2 import QtCore
+        from PySide2.QtCore import Qt, QEvent, QObject, QRect, QSettings, QTimer, QDir
+        from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QDockWidget, QMessageBox
+        from PySide2.QtGui import QCloseEvent, QIcon, QWindow,  QImage, QPixmap
+    except ImportError:
+        pass
